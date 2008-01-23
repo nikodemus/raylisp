643a00ac6123bc899d1d83e7d80c15757fa22bb0
Nikodemus Siivola <nikodemus@random-state.net>
2008-01-23 06:37:06 +0200

    usably fast GUI

--- gui.lisp
+++ gui.lisp
@@ -1,36 +1,71 @@
 (require :mcclim)
 
 (defpackage "RAYLISP-GUI"
-  (:use "CLIM-LISP" "CLIM")
+  (:use "CLIM-LISP" "CLIM" "CLIM-EXTENSIONS")
   (:import-from "RAYLISP" 
                 "@"
                 "ORIGIN"))
 
+(in-package "CLIMI")
+
+(in-package :climi)
+
+;;;; Image drawing
+
+(defgeneric clim::medium-draw-pixels* (medium array x y &key &allow-other-keys))
+(defgeneric clim::medium-get-pixels* (medium array x y &key width height &allow-other-keys))
+
+(export 'clim::medium-draw-pixels* :clim)
+(export 'clim::medium-get-pixels*  :clim)
+
+(in-package :clim-clx)
+
+;;; TODO: Extract indexed pattern drawing and convert to one of these functions.
+
+(defmethod clim::medium-draw-pixels* ((sheet sheet) array x y &key)
+  (with-sheet-medium (medium sheet)
+    (medium-draw-pixels* medium array x y)))
+
+(defmethod clim::medium-draw-pixels* 
+    ((medium clx-medium) array x y &key &allow-other-keys)
+  (let* ((width  (array-dimension array 1))
+         (height (array-dimension array 0))
+         (image (xlib:create-image :width width :height height :data array
+                                                               :bits-per-pixel 32
+                                                               :depth 24
+                                                               :format :z-pixmap)))
+    (with-clx-graphics (medium)
+      (xlib:put-image mirror gc image :x x :y y :width width :height height))))
+ 
+(defmethod clim::medium-get-pixels* 
+    ((medium clx-medium) array x y &key width height &allow-other-keys)
+  (let* ((width  (or width (array-dimension array 1)))
+         (height (or height (array-dimension array 0))))
+    (with-clx-graphics (medium)
+      (xlib:image-z-pixarray (xlib:get-image mirror :x x :y y :format :z-pixmap :width width :height height)))))
+
 (in-package "RAYLISP-GUI")
 
-(defun render-test ())
-
-(defun render-scene (scene medium)
-  (let* ((pixmap nil)
-         (region (sheet-region medium))
+(defun render-scene (scene sheet)
+  (let* ((region (sheet-region sheet))
          (width (bounding-rectangle-width region))
          (height (bounding-rectangle-height region))
-         (end (- width 1)))
-    (unwind-protect
-         (progn
-           (setf pixmap (allocate-pixmap medium width height))
-           (raylisp::render scene (raylisp::scene-default-camera scene)
-                            width height
-                            (lambda (color x y)
-                              (declare (type (simple-array single-float (3)) color)
-                                       (type fixnum x y))
-                              (let ((rgb (make-rgb-color (aref color 0) (aref color 1) (aref color 2))))
-                                (draw-point* pixmap x y :ink rgb)
-                                (when (= x end)
-                                  (copy-from-pixmap pixmap 0 y width 1 medium 0 y))))
-                            :normalize-camera t))
-      (when pixmap
-        (deallocate-pixmap pixmap)))))
+         (end (- width 1))
+         (image (make-array (list 1 width) :element-type '(unsigned-byte 32))))
+    (raylisp::render scene (raylisp::scene-default-camera scene)
+                     width height
+                     (lambda (color x y)
+                       (declare (type (simple-array single-float (3)) color)
+                                (type fixnum x y))
+                       ;; FIXME: Gamma...
+                       (let ((r (floor (* 255 (aref color 0))))
+                             (g (floor (* 255 (aref color 1))))
+                             (b (floor (* 255 (aref color 2)))))
+                         (setf (aref image 0 x) (logior (ash r 16) (ash g 8) b))
+                         (when (= x end)
+                           (with-sheet-medium (medium sheet)
+                             (medium-draw-pixels* medium image 0 y)))))
+                     :normalize-camera t)))
 
 (define-application-frame raylisp-frame ()
   ()
