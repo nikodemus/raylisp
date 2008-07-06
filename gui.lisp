9eba54064c3b43190e8e3f7b1b2d9fe68989eb07
Nikodemus Siivola <nikodemus@random-state.net>
2008-07-06 14:05:04 +0300

    grab-bag of gui work

--- gui.lisp
+++ gui.lisp
@@ -11,36 +11,72 @@
 
 (in-package "RAYLISP-GUI")
 
+(defun make-rgba-raster (width height)
+  (make-array (list height width) :element-type '(unsigned-byte 32)))
+
+(defun raster-height (raster)
+  (array-dimension raster 0))
+
+(defun raster-width (raster)
+  (array-dimension raster 1))
+
+(defun vector-rgba (vector)
+  (declare (raylisp::vector vector))
+  (flet ((dim (i)
+           (floor (* 255 (raylisp::clamp (aref vector i) 0 1)))))
+    (let ((r (dim 0))
+          (g (dim 1))
+          (b (dim 2)))
+      (logior (ash r 16) (ash g 8) b))))
+
+(defparameter *canvas-height* 400)
+(defparameter *canvas-width* 600)
+
+(define-application-frame raylisp-frame ()
+  ((image :initform (make-rgba-raster *canvas-height* *canvas-width*)
+          :accessor frame-image))
+  (:panes
+   (canvas :application :display-time nil
+           :height *canvas-height* :width *canvas-width*)
+   (repl :interactor :min-height 500))
+  (:layouts
+   (default (vertically (:width *canvas-width* :height (round (* 1.5 *canvas-height*)))
+              (2/3 canvas)
+              (:fill repl)))))
+
 (defun render-scene (scene sheet)
   (let* ((region (sheet-region sheet))
          (width (bounding-rectangle-width region))
          (height (bounding-rectangle-height region))
          (end (- width 1))
-         (image (make-array (list 1 width) :element-type '(unsigned-byte 32))))
+         (image (make-rgba-raster width height))
+         (row (make-rgba-raster width 1)))
+    (format t "~&Rendering ~S @ ~S x ~S~%" (raylisp::scene-name scene) width height)
     (raylisp::render scene (raylisp::scene-default-camera scene)
                      width height
                      (lambda (color x y)
                        (declare (type (simple-array single-float (3)) color)
                                 (type fixnum x y))
                        ;; FIXME: Gamma...
-                       (let ((r (floor (* 255 (aref color 0))))
-                             (g (floor (* 255 (aref color 1))))
-                             (b (floor (* 255 (aref color 2)))))
-                         (setf (aref image 0 x) (logior (ash r 16) (ash g 8) b))
-                         (when (= x end)
-                           (with-sheet-medium (medium sheet)
-                             (medium-draw-pixels* medium image 0 y)))))
-                     :normalize-camera t)))
+                       (let ((rgba (vector-rgba color)))
+                         (setf (aref row 0 x) rgba
+                               (aref image y x) rgba))
+                       (when (= x end)
+                         (with-sheet-medium (medium sheet)
+                           (medium-draw-pixels* medium row 0 y))))
+                     :normalize-camera t)
+    (setf (frame-image (pane-frame sheet)) image)))
 
-(define-application-frame raylisp-frame ()
-  ()
-  (:panes
-   (canvas :application :display-time nil :height 400 :width 600)
-   (repl :interactor))
-  (:layouts
-   (default (vertically (:width 600 :height 600)
-              canvas
-              (:fill repl)))))
+(defun shoot-ray-into-scene (scene sheet x y)
+  (let* ((region (sheet-region sheet))
+         (width (bounding-rectangle-width region))
+         (height (bounding-rectangle-height region))
+         (old (aref (frame-image (pane-frame sheet)) y x)))
+    (format t "~&Shooting ray into ~S @~Sx~S point ~S,~S~%" (raylisp::scene-name scene) width height x y)
+    (let ((color (raylisp::shoot-ray scene (raylisp::scene-default-camera scene)
+                               x y width height
+                               :normalize-camera t)))
+      (format t "~&Previous color: #x~X, current color: #x~X~%" old (vector-rgba color)))))
 
 (defmethod frame-standard-output ((frame raylisp-frame))
   (find-pane-named frame 'repl))
@@ -49,9 +85,16 @@
     ()
   (frame-exit *application-frame*))
 
+(define-raylisp-frame-command (com-update-raylisp :name t)
+    ()
+  (require :raylisp))
+
 (define-raylisp-frame-command (com-clear-canvas :name t)
     ()
-  (window-clear (find-pane-named *application-frame* 'canvas))
+  (window-clear (find-pane-named *application-frame* 'canvas)))
+
+(define-raylisp-frame-command (com-clear-repl :name t)
+    ()
   (window-clear (find-pane-named *application-frame* 'repl)))
 
 (defvar *last-scene-name* nil)
@@ -68,7 +111,6 @@
 (define-raylisp-frame-command (com-render-all :name t)
     ()
   (maphash (lambda (name scene)
-             (format t "Rendering ~A" name)
              (render-scene scene (find-pane-named *application-frame* 'canvas)))
            raylisp::*scenes*))
 
@@ -76,22 +118,38 @@
     ()
   (loop
     (maphash (lambda (name scene)
-               (format t "Rendering ~A" name)
                (render-scene scene (find-pane-named *application-frame* 'canvas)))
              raylisp::*scenes*)))
+
+(define-raylisp-frame-command (com-shoot-ray :name t)
+    ()
+  (let* ((name *last-scene-name*)
+         (scene (gethash name raylisp::*scenes*))
+         (canvas (find-pane-named *application-frame* 'canvas)))
+    (cond (scene
+           (block point
+             (format t "~&Click on the canvas to shoot a ray at that point.~%")
+             (tracking-pointer (*standard-output*)
+               (:pointer-button-press (&key event x y)
+                                      (when (eq canvas (event-sheet event))
+                                        (return-from point
+                                          (shoot-ray-into-scene scene canvas x y)))))))
+          (t
+           (if name
+               (format t "Oops: scene ~S seems to have vanished!~%" name)
+               (format t "No last scene to shoot a ray into!~%"))))))
 
 (define-raylisp-frame-command (com-toggle-kd :name t)
     ()
   (if (setf raylisp::*use-kd-tree* (not raylisp::*use-kd-tree*))
-      (format t "~&KD tree in use.~%")
-      (format t "~&KD tree not in used.~%")))
+      (format t "~&KD tree now in use.~%")
+      (format t "~&KD tree now not in use.~%")))
 
 (define-raylisp-frame-command (com-again :name t)
     ()
   (let* ((name *last-scene-name*)
          (scene (gethash name raylisp::*scenes*)))
     (cond (scene
-           (format t "Rendering ~A" name)
            (render-scene scene (find-pane-named *application-frame* 'canvas)))
           (t
            (format t "No scene named ~A" name)))))
