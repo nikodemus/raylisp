d9c68088051f28c3023b321cbd786ab74d0be236
Nikodemus Siivola <nikodemus@random-state.net>
2009-05-28 18:03:07 +0300

    sort out whitespace in clim-patch.lisp

--- clim-patch.lisp
+++ clim-patch.lisp
@@ -8,29 +8,34 @@
   (export 'clim::medium-draw-pixels* :clim)
   (export 'clim::medium-get-pixels*  :clim))
 
-(defgeneric clim::medium-draw-pixels* (medium array x y &key &allow-other-keys))
-(defgeneric clim::medium-get-pixels* (medium array x y &key width height &allow-other-keys))
+(defgeneric clim::medium-draw-pixels*
+    (medium array x y &key &allow-other-keys))
+(defgeneric clim::medium-get-pixels*
+    (medium array x y &key width height &allow-other-keys))
 
-;;; TODO: Extract indexed pattern drawing and convert to one of these functions.
+;;; TODO: Extract indexed pattern drawing and convert to one of these
+;;; functions.
 
 (defmethod clim::medium-draw-pixels* ((sheet sheet) array x y &key)
   (with-sheet-medium (medium sheet)
     (clim::medium-draw-pixels* medium array x y)))
 
-(defmethod clim::medium-draw-pixels* 
+(defmethod clim::medium-draw-pixels*
     ((medium clx-medium) array x y &key &allow-other-keys)
   (let* ((width  (array-dimension array 1))
          (height (array-dimension array 0))
          (image (xlib:create-image :width width :height height :data array
-                                                               :bits-per-pixel 32
-                                                               :depth 24
-                                                               :format :z-pixmap)))
+                                   :bits-per-pixel 32
+                                   :depth 24
+                                   :format :z-pixmap)))
     (with-clx-graphics (medium)
       (xlib:put-image mirror gc image :x x :y y :width width :height height))))
- 
-(defmethod clim::medium-get-pixels* 
+
+(defmethod clim::medium-get-pixels*
     ((medium clx-medium) array x y &key width height &allow-other-keys)
   (let* ((width  (or width (array-dimension array 1)))
          (height (or height (array-dimension array 0))))
     (with-clx-graphics (medium)
-      (xlib:image-z-pixarray (xlib:get-image mirror :x x :y y :format :z-pixmap :width width :height height)))))
+      (xlib:image-z-pixarray (xlib:get-image mirror :x x :y y
+                                             :width width :height height
+                                             :format :z-pixmap)))))
