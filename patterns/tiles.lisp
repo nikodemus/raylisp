79e6a3e5b606a0dd6b1bae1adc63b23f3d81725a
Nikodemus Siivola <nikodemus@random-state.net>
2009-07-20 16:05:18 +0300

    oops, missed the tiles.lisp from earlier commit

---
+++ patterns/tiles.lisp
@@ -0,0 +1,41 @@
+;;;; by Nikodemus Siivola <nikodemus@random-state.net>, 2009.
+;;;;
+;;;; Permission is hereby granted, free of charge, to any person
+;;;; obtaining a copy of this software and associated documentation files
+;;;; (the "Software"), to deal in the Software without restriction,
+;;;; including without limitation the rights to use, copy, modify, merge,
+;;;; publish, distribute, sublicense, and/or sell copies of the Software,
+;;;; and to permit persons to whom the Software is furnished to do so,
+;;;; subject to the following conditions:
+;;;;
+;;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;;;; IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+;;;; CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+;;;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+;;;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(in-package :raylisp)
+
+(defclass tiles-pattern (indexed-pattern)
+  ())
+
+(defmethod indexed-pattern-size ((pattern tiles-pattern))
+  4)
+
+(defmethod compute-pattern-key-function ((pattern tiles-pattern) transform)
+  (let ((inverse (inverse-matrix transform)))
+    (lambda (point)
+      (declare (optimize speed) (vec point))
+      (let ((p (transform-point point inverse)))
+        (declare (dynamic-extent p) (vec p))
+        (macrolet ((dim (n)
+                     `(ifloor (+ epsilon (aref p ,n)) 1.0)))
+          (if (oddp (dim 0))
+              (if (oddp (dim 2))
+                  0
+                  1)
+              (if (oddp (dim 2))
+                  2
+                  3)))))))
